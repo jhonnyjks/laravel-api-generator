4b29b6bdc534ef769c12dd4cbf49aad25ac0e376
Jhonny Mesquita <jhonnyjks@gmail.com>
2019-02-10 13:06:42 -0300

    Arquivo de rota reorganizado

--- routes/api.php
+++ routes/api.php
@@ -12,25 +12,18 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//Não mexer nesse grupo, a menos que queira alterar regras de autenticação
 Route::group([
 	'prefix' => 'auth'
 ], function () {
-
 	Route::post('signup', 'AuthController@signup');
-
-	// Route::group([
-	// 	'middleware' => 'auth:api'
-	// ], function() {
-		Route::post('login', [
-			'as' => 'api.auth.login',
-			'uses' => 'AuthController@login'
-		]);
-		Route::get('logout', 'AuthController@logout');
-		Route::get('user', 'AuthController@user');
-		Route::get('define_profile/{id}', 'AuthController@defineProfile');
-	// });
+	Route::post('login', [
+		'as' => 'api.auth.login',
+		'uses' => 'AuthController@login'
+	]);
+	Route::get('logout', 'AuthController@logout');
+	Route::get('user', 'AuthController@user');
+	Route::get('define_profile/{id}', 'AuthController@defineProfile');
 });
-
 
 Route::resource('persons', 'PersonAPIController');
